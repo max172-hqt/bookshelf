aaf767ef8b6faf99cce483d977f8fdec352fdd76
Huy Tran <max@gotitapp.co>
2021-07-06 18:01:58 +0700

    feat: Refactor book row

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {FullPageSpinner} from './components/lib'
-import {selectUser, selectIsLoading} from 'reducers/authSlice'
+import {selectUser} from 'reducers/authSlice'
 import {useSelector} from 'react-redux'
 
 const AuthenticatedApp = React.lazy(() =>
@@ -10,12 +10,9 @@ const UnauthenticatedApp = React.lazy(() => import('./unauthenticated-app'))
 
 function App() {
   const user = useSelector(selectUser)
-  const isLoading = useSelector(selectIsLoading)
 
-  if (isLoading) {
-    return <FullPageSpinner />
-  }
-
+  // TODO: Login page is blinking when refreshing the page
+  // if user is already authenticated
   return (
     <React.Suspense fallback={<FullPageSpinner />}>
       {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
